411486c0e972e49d505576dfa89255640847d5ee
Michael D. Mooring <mikeumus@gmail.com>
2012-12-14 03:25:45 -0500

    Adding ci.testling test. EXAMPLE STILL BROKEN.

---
+++ readme.markdown
@@ -0,0 +1,4 @@
+# lines
+
+Run lines tests in [all the browsers](http://browserling.com/browsers.json)
+with [testling-ci](http://ci.testling.com).
---
+++ test/test.js
@@ -0,0 +1,6 @@
+var test = require('tape');
+var myCode = require('../js/script.js');
+
+test('shpaes', function(shapes){
+    myCode.lines();
+});
